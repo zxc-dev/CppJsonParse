c50450fa67386c115e09be832fe6ca747c82e264
zxc <2218751654@qq.com>
2022-09-18 18:34:34 +0800

    111

--- json.h
+++ json.h
@@ -1,11 +1,14 @@
 #ifndef C94849BF_3285_494D_835D_033AB6BD9D6A
 #define C94849BF_3285_494D_835D_033AB6BD9D6A
 
-
+enum JsonType {
+    
+};
 class Json
 {
 private:
     /* data */
+    
 public:
     Json(/* args */);
     ~Json();
--- printer.cpp
+++ printer.cpp
@@ -2,19 +2,4 @@
 
 
 using namespace std;
-class Printer
-{
-private:
-    /* data */
-public:
-    Printer(/* args */);
-    ~Printer();
-};
 
-Printer::Printer(/* args */)
-{
-}
-
-Printer::~Printer()
-{
-}
